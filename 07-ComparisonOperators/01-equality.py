fe712321fbefb06d77d67ff5e2deb39b60088a38
Rudranil Maity <48630058+Pegasus-01@users.noreply.github.com>
2020-08-05 08:42:33 +0530

    Add files via upload

---
+++ 07-ComparisonOperators/01-equality.py
@@ -0,0 +1,17 @@
+# Comparison of booleans
+x=2
+y=3
+x<y
+
+# Comparison of integers
+a= -5*15
+b= 15
+c= 75
+(a*b)!=c
+
+# Comparison of strings
+True == False
+"pyscript"=="PyScript"
+
+# Compare a boolean with an integer
+True == 1
